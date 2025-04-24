2896a4e6cb2dbda04ac2579c4f8f04750c8850d2
Tom Gao <zimingg@andrew.cmu.edu>
2025-04-23 21:03:28 -0400

    Spawn multiple randomized peppers

--- src/SVD_sim_randomized_launcher.py
+++ src/SVD_sim_randomized_launcher.py
@@ -8,51 +8,84 @@ PREFIX="clear; roslaunch vader_hri vader_svd_dualsim.launch "
 
 PEPPER_SDF_FILE = "/home/docker_ws/src/vader_sim/src/xarm_gazebo/worlds/breakable_pepper.sdf"
 
-def generate_and_spawn():
-    # Generate random pose
+pepper_base_poses = [
+    [0.25, -0.6, 0.5],
+    [0.35, -0.55, 0.6],
+    [0.45, -0.5, 0.4],
+]
+
+def randomize_pepper(pepper_base_pose):
+    # Randomize the pose of the pepper robot
+    x = round(random.uniform(pepper_base_pose[0] - 0.05, pepper_base_pose[0] + 0.05), 2)
+    y = round(random.uniform(pepper_base_pose[1] - 0.05, pepper_base_pose[1] + 0.05), 2)
+    z = round(random.uniform(pepper_base_pose[2] - 0.05, pepper_base_pose[2] + 0.05), 2)
+    roll = round(random.uniform(-0.5, 0.5), 2)
+    pitch = round(random.uniform(-0.5, 0.5), 2)
+
     pose = {
-        "x": round(random.uniform(0.25, 0.45), 2),
-        "y": round(random.uniform(-0.45, -0.65), 2),
-        "z": round(random.uniform(0.4, 0.6), 2),
-        "roll": round(random.uniform(-0.5, 0.5), 2),
-        "pitch": round(random.uniform(-0.5, 0.5), 2)
-        # "x": 0.25,
-        # "y": -0.6,
-        # "z": 0.405,
-        # "roll": 0.4,
-        # "pitch": 0.1
+        "x": x,
+        "y": y,
+        "z": z,
+        "roll": roll,
+        "pitch": pitch
     }
+    return pose
 
-    # Below pose should add 1.101 to z, 0.696 to y
-    pose_gazebo = {
-        "x": round(0.5-pose["x"], 3),
-        "y": round(-0.51-pose["y"], 3),
-        "z": round(pose["z"] + 1.025, 3),
-        "roll": -pose["pitch"],
-        "pitch": pose["roll"]
+def get_gazebo_pose(pepper_pose):
+    # Convert the pepper pose to gazebo pose
+    gazebo_pose = {
+        "x": round(0.5 - pepper_pose["x"], 3),
+        "y": round(-0.51 - pepper_pose["y"], 3),
+        "z": round(pepper_pose["z"] + 1.025, 3),
+        "roll": -pepper_pose["pitch"],
+        "pitch": pepper_pose["roll"]
     }
+    return gazebo_pose
 
-    arg_sim_pepper_pose = f"sim_pepper_pose:=\"{pose['x']} {pose['y']} {pose['z']} {pose['roll']} {pose['pitch']} 0.0\""
+def generate_and_spawn():
+    randomized_peppers = []
+    for pepper in pepper_base_poses:
+        pose = randomize_pepper(pepper)
+        randomized_peppers.append(pose)
+
+    # pose = randomize_pepper(pepper_base_poses[0])
+    pose_gazebo = get_gazebo_pose(randomized_peppers[0])
+
+    arg_sim_pepper_pose = f"sim_pepper_pose:=\"{randomized_peppers[0]['x']} {randomized_peppers[0]['y']} {randomized_peppers[0]['z']} {randomized_peppers[0]['roll']} {randomized_peppers[0]['pitch']} 0.0\""
     arg_gazebo_sim_pepper_pose = f"gazebo_sim_pepper_pose:=\"{pose_gazebo['x']} {pose_gazebo['y']} {pose_gazebo['z']} {pose_gazebo['roll']} {pose_gazebo['pitch']} 0.0\""
 
     hri_launch_command = PREFIX + arg_sim_pepper_pose + " " + arg_gazebo_sim_pepper_pose
 
-    gazebo_model_name = "pepper1"
-    gazebo_command = "rosrun gazebo_ros spawn_model -gazebo_namespace /gazebo -file "
-    gazebo_command += PEPPER_SDF_FILE
-    gazebo_command += f" -x {pose_gazebo['x']} -y {pose_gazebo['y']} -z {pose_gazebo['z']} -R {pose_gazebo['roll']} -P {pose_gazebo['pitch']} -sdf -model {gazebo_model_name}"
+    gazebo_spawn_procs = []
+    for i in range(len(pepper_base_poses)):
+        pose_gazebo = get_gazebo_pose(randomized_peppers[i])
+        gazebo_model_name = "pepper" + str(i)
+        gazebo_command = "rosrun gazebo_ros spawn_model -gazebo_namespace /gazebo -file "
+        gazebo_command += PEPPER_SDF_FILE
+        gazebo_command += f" -x {pose_gazebo['x']} -y {pose_gazebo['y']} -z {pose_gazebo['z']} -R {pose_gazebo['roll']} -P {pose_gazebo['pitch']} -sdf -model {gazebo_model_name}"
+        gazebo_spawn_procs.append(subprocess.Popen([gazebo_command], shell=True))
+    # gazebo_model_name = "pepper1"
+    # gazebo_command = "rosrun gazebo_ros spawn_model -gazebo_namespace /gazebo -file "
+    # gazebo_command += PEPPER_SDF_FILE
+    # gazebo_command += f" -x {pose_gazebo['x']} -y {pose_gazebo['y']} -z {pose_gazebo['z']} -R {pose_gazebo['roll']} -P {pose_gazebo['pitch']} -sdf -model {gazebo_model_name}"
 
     hri_proc = subprocess.Popen([hri_launch_command], shell=True)
-    gazebo_proc = subprocess.Popen([gazebo_command], shell=True)
+    # gazebo_proc = subprocess.Popen([gazebo_command], shell=True)
 
     try:
         hri_proc.wait()
-        gazebo_proc.wait()
+        for proc in gazebo_spawn_procs:
+            proc.wait()
+        # gazebo_proc.wait()
     except KeyboardInterrupt:
         hri_proc.terminate()
-        gazebo_proc.terminate()
+        for proc in gazebo_spawn_procs:
+            proc.terminate()
+        # gazebo_proc.terminate()
         hri_proc.wait()
-        gazebo_proc.wait()
+        for proc in gazebo_spawn_procs:
+            proc.wait()
+        # gazebo_proc.wait()
 
 
 if __name__ == "__main__":
