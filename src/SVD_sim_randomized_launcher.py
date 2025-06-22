ecf5571a34c4e42238b799a51e3e14b0b41ea6a7
Tom Gao <zimingg@andrew.cmu.edu>
2025-06-22 19:43:38 -0400

    (sim) gazebo coordinate frame is now the same as moveit planning frame

--- src/SVD_sim_randomized_launcher.py
+++ src/SVD_sim_randomized_launcher.py
@@ -1,30 +1,25 @@
 #!/usr/bin/env python3
 import random
 
-PREFIX="clear; roslaunch vader_hri vader_svd_dualsim.launch "
+PREFIX="clear; roslaunch vader_hri dualsim_noplan.launch "
 
 def generate_poses():
     # Generate random pose
     pose = {
-        "x": round(random.uniform(0.25, 0.45), 2),
-        "y": round(random.uniform(-0.45, -0.65), 2),
+        "x": round(random.uniform(0.45, 0.55), 2), #depth from arm reach
+        "y": round(random.uniform(0.35, 0.55), 2), #left from x-axis
         "z": round(random.uniform(0.4, 0.6), 2),
         "roll": round(random.uniform(-0.5, 0.5), 2),
         "pitch": round(random.uniform(-0.5, 0.5), 2)
-        # "x": 0.25,
-        # "y": -0.6,
-        # "z": 0.405,
-        # "roll": 0.4,
-        # "pitch": 0.1
     }
 
     # Below pose should add 1.101 to z, 0.696 to y
     pose_gazebo = {
-        "x": round(0.5-pose["x"], 3),
-        "y": round(-0.51-pose["y"], 3),
-        "z": round(pose["z"] + 1.025, 3),
-        "roll": -pose["pitch"],
-        "pitch": pose["roll"]
+        "x": round(pose["x"], 3),
+        "y": round(pose["y"], 3),
+        "z": round(pose["z"], 3),
+        "roll": pose["roll"],
+        "pitch": pose["pitch"]
     }
 
     arg_sim_pepper_pose = f"sim_pepper_pose:=\"{pose['x']} {pose['y']} {pose['z']} {pose['roll']} {pose['pitch']} 0.0\""
