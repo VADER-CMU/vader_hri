259cc67469e2c01da7a26b71a94f6a2ecdfa367b
Tom Gao <zimingg@andrew.cmu.edu>
2025-10-25 11:03:34 -0400

    Fixing up SVD sim launcher with new fvd configs

---
+++ src/FVD_Dual_HRI.cpp
@@ -0,0 +1,316 @@
+#include <iostream>
+
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include "vader_msgs/Pepper.h"
+// #include "vader_msgs/BimanualPlanRequest.h"
+// #include "vader_msgs/BimanualExecRequest.h"
+// #include "vader_msgs/MoveToStorageRequest.h"
+#include "vader_msgs/GripperCommand.h"
+#include "vader_msgs/CutterCommand.h"
+
+#include "vader_msgs/PlanningRequest.h"
+
+#include <ros/ros.h>
+#include <tf2_ros/transform_listener.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <tf2_ros/buffer_interface.h>
+#include <tf2_ros/buffer.h>
+#include <geometry_msgs/PoseStamped.h>
+
+static void coarseEstimateCallback(const vader_msgs::Pepper::ConstPtr &msg);
+static void fineEstimateCallback(const vader_msgs::Pepper::ConstPtr &msg);
+
+class VADERStateMachine
+{
+private:
+    enum State
+    {
+        // ----------Coarse-Pregrasp stage-----------
+        HomeGripper,
+        HomeCutter,
+        WaitForCoarseEstimate,
+        ParallelMovePregrasp,
+        // ----------Fine-Grasp stage-----------
+        WaitForFineEstimate,
+        GripperGrasp,
+        // ------------Harvest stage-----------
+        CutterGrasp,
+        // -----------Finish and cleanup-----------
+        ParallelMoveStorage,
+        Done,
+        Error
+    };
+
+    State currentState;
+    ros::NodeHandle n;
+
+    // Perception subscriptions and data
+    ros::Subscriber coarseEstimateSub, fineEstimateSub;
+    vader_msgs::Pepper *coarseEstimate;
+    vader_msgs::Pepper *fineEstimate;
+
+    // Plan/Exec clients connecting to planner
+    ros::ServiceClient planClient;
+
+    // End effector talking to dynamixel node
+    ros::Publisher gripperCommandPub, cutterCommandPub;
+
+    /*
+    Input: cameraFrameMsg containing the pose of the fruit in the camera frame from the coarse/fine pepper estimate
+    Output: result containing the pose of the fruit in the world frame using TF2.
+    */
+    void _transformFromCameraFrameIntoRobotFrame(const vader_msgs::Pepper::ConstPtr &cameraFrameMsg, vader_msgs::Pepper *result)
+    {
+        tf2_ros::Buffer tf_buffer;
+        tf2_ros::TransformListener tf_listener(tf_buffer);
+        geometry_msgs::PoseStamped fruit_pose;
+        //TODO The below x/y swap/weirdness is a weird artifact left from previous PR4 work. Should be removed, but also need ot change sim logic everywhere
+        //So that the coordinates are correct with x/y unswapped.
+        fruit_pose.pose.position.x = -cameraFrameMsg->fruit_data.pose.position.y;
+        fruit_pose.pose.position.y = cameraFrameMsg->fruit_data.pose.position.x;
+        fruit_pose.pose.position.z = cameraFrameMsg->fruit_data.pose.position.z;
+        fruit_pose.pose.orientation.x = cameraFrameMsg->fruit_data.pose.orientation.x;
+        fruit_pose.pose.orientation.y = cameraFrameMsg->fruit_data.pose.orientation.y;
+        fruit_pose.pose.orientation.z = cameraFrameMsg->fruit_data.pose.orientation.z;
+        fruit_pose.pose.orientation.w = cameraFrameMsg->fruit_data.pose.orientation.w;
+
+        geometry_msgs::PoseStamped peduncle_pose;
+        peduncle_pose.pose.position.x = -cameraFrameMsg->peduncle_data.pose.position.y;
+        peduncle_pose.pose.position.y = cameraFrameMsg->peduncle_data.pose.position.x;
+        peduncle_pose.pose.position.z = cameraFrameMsg->peduncle_data.pose.position.z;
+        peduncle_pose.pose.orientation.x = cameraFrameMsg->peduncle_data.pose.orientation.x;
+        peduncle_pose.pose.orientation.y = cameraFrameMsg->peduncle_data.pose.orientation.y;
+        peduncle_pose.pose.orientation.z = cameraFrameMsg->peduncle_data.pose.orientation.z;
+        peduncle_pose.pose.orientation.w = cameraFrameMsg->peduncle_data.pose.orientation.w;
+        fruit_pose.header.frame_id = cameraFrameMsg->header.frame_id;
+        peduncle_pose.header.frame_id = cameraFrameMsg->header.frame_id;
+
+        try
+        {
+            geometry_msgs::PoseStamped transformed_pose = tf_buffer.transform(
+                fruit_pose,
+                "link_base",
+                ros::Duration(3.0));
+            geometry_msgs::PoseStamped transformed_peduncle_pose = tf_buffer.transform(
+                peduncle_pose,
+                "link_base",
+                ros::Duration(3.0));
+            // ROS_INFO("Transformed pose: x=%f, y=%f, z=%f",
+            //          transformed_pose.pose.position.x,
+            //          transformed_pose.pose.position.y,
+            //  transformed_pose.pose.position.z);
+            result->fruit_data.pose.position.x = transformed_pose.pose.position.x;
+            result->fruit_data.pose.position.y = transformed_pose.pose.position.y;
+            result->fruit_data.pose.position.z = transformed_pose.pose.position.z;
+            result->fruit_data.pose.orientation.x = transformed_pose.pose.orientation.x;
+            result->fruit_data.pose.orientation.y = transformed_pose.pose.orientation.y;
+            result->fruit_data.pose.orientation.z = transformed_pose.pose.orientation.z;
+            result->fruit_data.pose.orientation.w = transformed_pose.pose.orientation.w;
+            result->fruit_data.shape.dimensions.resize(2);
+            result->fruit_data.shape.dimensions[0] = cameraFrameMsg->fruit_data.shape.dimensions[0];
+            result->fruit_data.shape.dimensions[1] = cameraFrameMsg->fruit_data.shape.dimensions[1];
+
+            result->peduncle_data.pose.position.x = transformed_peduncle_pose.pose.position.x;
+            result->peduncle_data.pose.position.y = transformed_peduncle_pose.pose.position.y;
+            result->peduncle_data.pose.position.z = transformed_peduncle_pose.pose.position.z;
+            result->peduncle_data.pose.orientation.x = transformed_peduncle_pose.pose.orientation.x;
+            result->peduncle_data.pose.orientation.y = transformed_peduncle_pose.pose.orientation.y;
+            result->peduncle_data.pose.orientation.z = transformed_peduncle_pose.pose.orientation.z;
+            result->peduncle_data.pose.orientation.w = transformed_peduncle_pose.pose.orientation.w;
+            result->peduncle_data.shape.dimensions.resize(2);
+            result->peduncle_data.shape.dimensions[0] = cameraFrameMsg->peduncle_data.shape.dimensions[0];
+            result->peduncle_data.shape.dimensions[1] = cameraFrameMsg->peduncle_data.shape.dimensions[1];
+        }
+        catch (tf2::TransformException &ex)
+        {
+            ROS_WARN("Transform failed: %s", ex.what());
+        }
+    }
+
+    void _sendGripperCommand(int open_pct)
+    {
+        vader_msgs::GripperCommand gripperCommand;
+        gripperCommand.open_pct = open_pct;
+        gripperCommandPub.publish(gripperCommand);
+        ROS_INFO_STREAM("Gripper command: open_pct=" << open_pct);
+    }
+
+    void _sendCutterCommand(int open_pct)
+    {
+        vader_msgs::CutterCommand cutterCommand;
+        cutterCommand.open_pct = open_pct;
+        cutterCommandPub.publish(cutterCommand);
+        ROS_INFO_STREAM("Cutter command: open_pct=" << open_pct);
+    }
+
+    void _logWithState(const std::string &message)
+    {
+        ROS_INFO_STREAM("[" << currentState << "]: " << message);
+    }
+
+public:
+    VADERStateMachine() : currentState(State::HomeGripper)
+    {
+        coarseEstimate = nullptr;
+        coarseEstimateSub = n.subscribe("/fruit_coarse_pose", 10, coarseEstimateCallback);
+        fineEstimate = nullptr;
+        fineEstimateSub = n.subscribe("/fruit_fine_pose", 10, fineEstimateCallback);
+
+        planClient = n.serviceClient<vader_msgs::PlanningRequest>("vader_planning_service");
+
+        gripperCommandPub = n.advertise<vader_msgs::GripperCommand>("/gripper_command", 10);
+        cutterCommandPub = n.advertise<vader_msgs::CutterCommand>("/cutter_command", 10);
+    }
+
+    void setCoarsePoseEstimate(const vader_msgs::Pepper::ConstPtr &msg)
+    {
+        coarseEstimate = new vader_msgs::Pepper();
+        coarseEstimate->header = msg->header;
+        _transformFromCameraFrameIntoRobotFrame(msg, coarseEstimate);
+        // _logWithState("Coarse estimate received");
+    }
+
+    void setFinePoseEstimate(const vader_msgs::Pepper::ConstPtr &msg)
+    {
+        fineEstimate = new vader_msgs::Pepper();
+        fineEstimate->header = msg->header;
+        _transformFromCameraFrameIntoRobotFrame(msg, fineEstimate);
+        // _logWithState("Fine estimate received");
+    }
+
+    void execute()
+    {
+        ros::Rate loop_rate(10);
+        while (ros::ok())
+        {
+            switch (currentState)
+            {
+                case State::HomeGripper:{
+                    _logWithState("Homing gripper...");
+                    vader_msgs::PlanningRequest srv;
+                    srv.request.mode = vader_msgs::PlanningRequest::Request::HOME_GRIPPER;
+                    if (planClient.call(srv))
+                    {
+                        if (srv.response.success)
+                        {
+                            _logWithState("Gripper homed successfully.");
+                            currentState = State::HomeCutter;
+                        }
+                        else
+                        {
+                            _logWithState("Gripper homing failed.");
+                            currentState = State::Error;
+                        }
+                    }
+                    else
+                    {
+                        _logWithState("Failed to call planning service for gripper homing.");
+                        currentState = State::Error;
+                    }
+                    break;
+                }
+                case State::HomeCutter:{
+                    _logWithState("Homing cutter...");
+                    vader_msgs::PlanningRequest srv;
+                    srv.request.mode = vader_msgs::PlanningRequest::Request::HOME_CUTTER;
+                    if (planClient.call(srv))
+                    {
+                        if (srv.response.success)
+                        {
+                            _logWithState("Cutter homed successfully.");
+                            currentState = State::WaitForCoarseEstimate;
+                        }
+                        else
+                        {
+                            _logWithState("Cutter homing failed.");
+                            currentState = State::Error;
+                        }
+                    }
+                    else
+                    {
+                        _logWithState("Failed to call planning service for cutter homing.");
+                        currentState = State::Error;
+                    }
+                    break;
+                }
+                case State::WaitForCoarseEstimate:{
+                    if (coarseEstimate != nullptr)
+                    {
+                        _logWithState("Coarse estimate received, switching states");
+                        currentState = State::Done;
+                    }
+                    else
+                    {
+                        _logWithState("Waiting for coarse estimate");
+                    }
+                    break;
+                }
+                case State::ParallelMovePregrasp:{
+
+                }
+                case State::WaitForFineEstimate:{
+
+                }
+                case State::GripperGrasp:{
+
+                }
+                case State::CutterGrasp:{
+
+                }
+                case State::ParallelMoveStorage:{
+
+                }
+                case State::Done:
+                {
+                    // ROS_INFO("Done");
+                    _logWithState("Done");
+                    break;
+                }
+                case State::Error:
+                {
+                    // ROS_INFO("Error");
+                    break;
+                }
+            }
+            ros::spinOnce();
+            loop_rate.sleep();
+        }
+    }
+};
+
+VADERStateMachine *sm = nullptr;
+
+static void coarseEstimateCallback(const vader_msgs::Pepper::ConstPtr &msg)
+{
+    if (sm != nullptr)
+    {
+        sm->setCoarsePoseEstimate(msg);
+    }
+    else
+    {
+        ROS_ERROR("State machine is not initialized");
+    }
+}
+
+static void fineEstimateCallback(const vader_msgs::Pepper::ConstPtr &msg)
+{
+    if (sm != nullptr)
+    {
+        sm->setFinePoseEstimate(msg);
+    }
+    else
+    {
+        ROS_ERROR("State machine is not initialized");
+    }
+}
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "vader_hri");
+    VADERStateMachine statemachine = VADERStateMachine();
+    sm = &statemachine;
+    ros::Duration(10.0).sleep(); //wait for stuff
+    statemachine.execute();
+    return 0;
+}
--- src/SVD_sim_randomized_launcher.py
+++ src/SVD_sim_randomized_launcher.py
@@ -1,21 +1,143 @@
 #!/usr/bin/env python3
 import random
+import subprocess
+# import os
+import time
+from vader_msgs.msg import SimulationPepperSequence, SimulationPopPepper
+from geometry_msgs.msg import Pose
+from tf.transformations import quaternion_from_euler
+import rospy
 
-PREFIX="clear; roslaunch vader_hri dualsim_noplan.launch "
+PREFIX="clear; roslaunch vader_hri vader_fvd_dualsim.launch "
 
-def generate_poses():
-    # Generate random pose
-    pose = {
-        "x": round(random.uniform(0.45, 0.55), 3), #depth from arm reach
-        "y": round(random.uniform(0.35, 0.55), 3), #left from x-axis
-        "z": round(random.uniform(0.4, 0.6), 3),
-        "roll": round(random.uniform(-0.5, 0.5), 3),
-        "pitch": round(random.uniform(-0.5, 0.5), 3)
+PEPPER_SDF_FILE = "/home/docker_ws/src/vader_sim/src/xarm_gazebo/worlds/breakable_pepper.sdf"
+
+pepper_base_poses = [
+    [0.2, -0.5, 0.5],
+    [0.5, -0.6, 0.4],
+    [0.35, -0.55, 0.65],
+]
+
+def randomize_pepper(pepper_base_pose):
+    # Randomize the pose of the pepper robot
+    x = round(random.uniform(pepper_base_pose[0] - 0.05, pepper_base_pose[0] + 0.05), 2)
+    y = round(random.uniform(pepper_base_pose[1] - 0.05, pepper_base_pose[1] + 0.05), 2)
+    z = round(random.uniform(pepper_base_pose[2] - 0.05, pepper_base_pose[2] + 0.05), 2)
+    roll = round(random.uniform(-0.5, 0.5), 2)
+    pitch = round(random.uniform(-0.5, 0.5), 2)
+
+    pepper_pose_dict = {
+        "x": x,
+        "y": y,
+        "z": z,
+        "roll": roll,
+        "pitch": pitch
     }
 
-    arg_sim_pepper_pose = f"sim_pepper_pose:=\"{pose['x']} {pose['y']} {pose['z']} {pose['roll']} {pose['pitch']} 0.0\""
+    pose = Pose()
+    pose.position.x = x
+    pose.position.y = y
+    pose.position.z = z
+    quaternion = quaternion_from_euler(roll, pitch, 0)
+    pose.orientation.x = quaternion[0]
+    pose.orientation.y = quaternion[1]
+    pose.orientation.z = quaternion[2]
+    pose.orientation.w = quaternion[3]
 
-    print(PREFIX + arg_sim_pepper_pose)
+
+    return pepper_pose_dict, pose
+
+def get_gazebo_pose(pepper_pose):
+    # Convert the pepper pose to gazebo pose
+    gazebo_pose = {
+        "x": round(0.5 - pepper_pose["x"], 3),
+        "y": round(-0.51 - pepper_pose["y"], 3),
+        "z": round(pepper_pose["z"] + 1.025, 3),
+        "roll": -pepper_pose["pitch"],
+        "pitch": pepper_pose["roll"]
+    }
+    return gazebo_pose
+
+def generate_and_spawn():
+    randomized_peppers = []
+    pepper_poses = []
+    for pepper in pepper_base_poses:
+        pose_dict, pose_msg = randomize_pepper(pepper)
+        randomized_peppers.append(pose_dict)
+        pepper_poses.append(pose_msg)
+
+    # pose = randomize_pepper(pepper_base_poses[0])
+    pose_gazebo = get_gazebo_pose(randomized_peppers[0])
+
+    arg_sim_pepper_pose = f"sim_pepper_pose:=\"{randomized_peppers[0]['x']} {randomized_peppers[0]['y']} {randomized_peppers[0]['z']} {randomized_peppers[0]['roll']} {randomized_peppers[0]['pitch']} 0.0\""
+    arg_gazebo_sim_pepper_pose = f"gazebo_sim_pepper_pose:=\"{pose_gazebo['x']} {pose_gazebo['y']} {pose_gazebo['z']} {pose_gazebo['roll']} {pose_gazebo['pitch']} 0.0\""
+
+    hri_launch_command = PREFIX + arg_sim_pepper_pose + " " + arg_gazebo_sim_pepper_pose
+
+    
+    gazebo_spawn_procs = []
+    for i in range(len(pepper_base_poses)):
+        pose_gazebo = get_gazebo_pose(randomized_peppers[i])
+        gazebo_model_name = "pepper" + str(i)
+        gazebo_command = "rosrun gazebo_ros spawn_model -gazebo_namespace /gazebo -file "
+        gazebo_command += PEPPER_SDF_FILE
+        gazebo_command += f" -x {pose_gazebo['x']} -y {pose_gazebo['y']} -z {pose_gazebo['z']} -R {pose_gazebo['roll']} -P {pose_gazebo['pitch']} -sdf -model {gazebo_model_name}"
+        gazebo_spawn_procs.append(subprocess.Popen([gazebo_command], shell=True))
+    # gazebo_model_name = "pepper1"
+    # gazebo_command = "rosrun gazebo_ros spawn_model -gazebo_namespace /gazebo -file "
+    # gazebo_command += PEPPER_SDF_FILE
+    # gazebo_command += f" -x {pose_gazebo['x']} -y {pose_gazebo['y']} -z {pose_gazebo['z']} -R {pose_gazebo['roll']} -P {pose_gazebo['pitch']} -sdf -model {gazebo_model_name}"
+
+    hri_proc = subprocess.Popen([hri_launch_command], shell=True)
+
+    rospy.init_node("SVD_sim_randomized_launcher", anonymous=True)
+    time.sleep(1)
+    pepper_seq_pub = rospy.Publisher("/pepper_sequence", SimulationPepperSequence, queue_size=10)
+    for i in range(3):
+        time.sleep(.5)
+        pepper_seq_msg = SimulationPepperSequence()
+        pepper_seq_msg.pepper_poses = pepper_poses
+        pepper_seq_pub.publish(pepper_seq_msg)
+
+    # gazebo_proc = subprocess.Popen([gazebo_command], shell=True)
+
+    try:
+        hri_proc.wait()
+        for proc in gazebo_spawn_procs:
+            proc.wait()
+        # gazebo_proc.wait()
+    except KeyboardInterrupt:
+        hri_proc.terminate()
+        for proc in gazebo_spawn_procs:
+            proc.terminate()
+        # gazebo_proc.terminate()
+        hri_proc.wait()
+        for proc in gazebo_spawn_procs:
+            proc.wait()
+        # gazebo_proc.wait()
+
 
 if __name__ == "__main__":
-    generate_poses()
+    generate_and_spawn()
+
+# #!/usr/bin/env python3
+# import random
+
+# PREFIX="clear; roslaunch vader_hri vader_fvd_dualsim.launch "
+
+# def generate_poses():
+#     # Generate random pose
+#     pose = {
+#         "x": round(random.uniform(0.45, 0.55), 3), #depth from arm reach
+#         "y": round(random.uniform(0.35, 0.55), 3), #left from x-axis
+#         "z": round(random.uniform(0.4, 0.6), 3),
+#         "roll": round(random.uniform(-0.5, 0.5), 3),
+#         "pitch": round(random.uniform(-0.5, 0.5), 3)
+#     }
+
+#     arg_sim_pepper_pose = f"sim_pepper_pose:=\"{pose['x']} {pose['y']} {pose['z']} {pose['roll']} {pose['pitch']} 0.0\""
+
+#     print(PREFIX + arg_sim_pepper_pose)
+
+# if __name__ == "__main__":
+#     generate_poses()
